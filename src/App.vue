4fb24038ed53584d34c3d6a67bdc5e4125440405
Erwin Macaraig <erwin.macaraig@enablesme.com.au>
2022-06-09 14:04:46 +0800

    implemented design

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <script setup>
-
+import NavBar from '@/components/Layout/NavBar.vue'
+  
 </script>
 
 <template>
-  <RouterLink to="/">Notes</RouterLink> | 
-  <RouterLink to="/stats">Stats</RouterLink>
-
-  <RouterView />
+  <!-- <RouterLink to="/" class="button">Notes</RouterLink> | 
+  <RouterLink to="/stats" class="button">Stats</RouterLink> -->
+  <NavBar />
+  <div class="container is-max-desktop px-2 py-4">
+    <RouterView />
+  </div>
+  
 </template>
 
 <style>
+@import 'bulma/css/bulma.min.css'
 
 </style>
---
+++ src/components/Layout/NavBar.vue
@@ -0,0 +1,69 @@
+<template>
+    <nav class="navbar is-success" aria-label="main navigation" role="navigation">
+        <div class="container is-max-desktop px-2">
+            <div class="navbar-brand">
+                <div class="navbar-item is-size-4 is-family-monospace">
+                    Noteballs
+                </div>
+
+                <a @click.prevent="showMobileNav =!showMobileNav" class="navbar-burger" :class="{ 'is-active': showMobileNav}" aria-expanded="false" aria-label="menu"
+                    data-target="navbarBasicExample" role="button">
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                </a>
+            </div>
+
+            <div id="navbarBasicExample" class="navbar-menu" :class="{ 'is-active': showMobileNav}">
+                <!-- <div class="navbar-start">
+                <div class="navbar-item has-dropdown is-hoverable">
+                    <a class="navbar-link">
+                        More
+                    </a>
+
+                    <div class="navbar-dropdown">
+                        <a class="navbar-item">
+                            About
+                        </a>
+                        <a class="navbar-item">
+                            Jobs
+                        </a>
+                        <a class="navbar-item">
+                            Contact
+                        </a>
+                        <hr class="navbar-divider">
+                        <a class="navbar-item">
+                            Report an issue
+                        </a>
+                    </div>
+                </div>
+            </div> -->
+
+                <div class="navbar-end">
+                    <RouterLink to="/" class="navbar-item" active-class="is-active">
+                        Notes
+                    </RouterLink>
+                    <RouterLink to="/stats" class="navbar-item" active-class="is-active">
+                        Stats
+                    </RouterLink>
+                </div>
+            </div>
+        </div>
+
+    </nav>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const showMobileNav = ref(false)
+</script>
+
+<style>
+@media (max-width:1023px){
+    .navbar-menu {
+        position: absolute;
+        left: 0;
+        width: 100%;
+    }
+}
+</style>
